7bca7c02ef19dd73899e6233164acec2b4579d18
Beniamin Abramowicz <beniamin.abramowicz@gmail.com>
2019-10-31 15:55:46 +0100

    Added css file with custom styles, created new style bundle, using table to display each order

--- App_Start/BundleConfig.cs
+++ App_Start/BundleConfig.cs
@@ -13,6 +13,7 @@ namespace ASPNETapp2
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
+            bundles.Add(new StyleBundle("~/Content/mystyles").Include("~/Content/mystyles.css"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
